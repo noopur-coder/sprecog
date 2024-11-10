2ab26f18767584c29cf4dd002a7a79a1d83ebd03
noopur-coder <noopursaini22@gmail.com>
2024-11-10 16:09:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,94 +1,98 @@
+# Before using the code open terminal and install these packages
+
+
 import streamlit as st
-from streamlit_ace import st_ace
 import speech_recognition as sr
-from wit import Wit
 
-r = sr.Recognizer()
-access_token="56MHVINSJPQIMHIHPGZCOHTPU64ZJUXY"
-client = Wit(access_token)
-intent=""
-entity=""
+# Create a dictionary to map language names to language codes
+language_codes = {
+    "English": "en-US",
+    "French": "fr-FR",
+    "Spanish": "es-ES"
+}
 
-LANGUAGES = [
-   "python","abp","java"
-]
-
-THEMES = [
-    "github","ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn",
-    "dracula", "dreamweaver", "eclipse","gob", "gruvbox", "idle_fingers", "iplastic",
-    "katzenmilch", "kr_theme", "kuroir", "merbivore", "merbivore_soft", "mono_industrial", "monokai",
-    "nord_dark", "pastel_on_dark", "solarized_dark", "solarized_light", "sqlserver", "terminal",
-    "textmate", "tomorrow", "tomorrow_night", "tomorrow_night_blue", "tomorrow_night_bright",
-    "tomorrow_night_eighties", "twilight", "vibrant_ink", "xcode"
-]
-
-KEYBINDINGS = [
-    "sublime","emacs","vim", "vscode"
-]
-
-def output(intent,entity):
-    if intent == 'create_list':
-        return f"{entity} = []"
-    elif intent=="range":
-        return f"range({entity})" 
-    elif intent=="sort_ascending":
-        return f"sorted_list = {entity}.sort()"
-    elif intent=="list_max":
-        return f"max_element=max({entity})"
-    elif intent=="list_len":
-        return f"length=len({entity})" 
-    elif intent=="list_reverse":
-        return f"reverse_list={entity}.reverse()"
+def transcribe_speech(api="Google Web Speech API", saved='No', lang="English"):
+    # Initialize recognizer class
+    # Creating a Recognizer instance
+    r = sr.Recognizer()
+    
+    # Creating an instance of the Microphone class
+    mic = sr.Microphone()
+    
+    # Configure the language using language codes
+    if api == "Google Web Speech API":
+        language_code = language_codes[lang]
     else:
-        return "#Instruct with voice" 
-
-
-st.title('VOCAI')
-st.sidebar.title(":memo: Voice Code Editor :smile:") 
-st.text('Editor')   
-content = st_ace(
-        placeholder= "Enter your code",
-        height = 250,
-        language=st.sidebar.selectbox("Select Language.", options=LANGUAGES),
-        theme=st.sidebar.selectbox("Editor Theme.", options=THEMES),
-        keybinding=st.sidebar.selectbox("Keybinding mode.", options=KEYBINDINGS),
-        font_size=st.sidebar.slider("Font size.", 5, 24, 12),
-        tab_size=st.sidebar.slider("Tab size.", 1, 8, 4),
-        show_gutter=st.sidebar.checkbox("Show gutter.", value=True),
-        show_print_margin=st.sidebar.checkbox("Show print margin.", value=True),
-        key="ace-editor1",
-        wrap=True
-    )
-
-MyText=""
-with open("code.txt") as file:
-    MyText = file.read()
-
-if st.button('Start recording'):
-    try:
-        with sr.Microphone() as source2:
-            with st.spinner('Recording Started...'):
-                r.adjust_for_ambient_noise(source2)
-                audio2 = r.listen(source2)
-                MyText = r.recognize_google(audio2)
-                response = client.message(MyText)
-                intent = response['intents'][0]['name']
-                entity = response['entities']['list_name:list_name'][0]['value']
-                #print(response)
-
-                with open("code.txt","w") as file:
-                    file.write(output(intent,entity))
+        language_code = None
+    
+    # Reading Microphone as source
+    with mic as source:
+        # Handle ambient noise
+        r.adjust_for_ambient_noise(source, duration=0.5)
         
-    except:
-        st.text('Error recording your voice. Please try again')    
+        st.info("Speak now...")
+        
+        # listen for speech and store in audio_text variable
+        recorded_audio = r.listen(source, timeout=None)  # Use timeout=None to listen indefinitely
+        
+        st.info("Transcribing...")
 
-if len(MyText)>0:
-    st_ace(value=output(intent,entity),height=50)
+        if api=="Google Web Speech API":
+            try:
+                # for testing purposes, we're just using the default API key
+                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+                # instead of `r.recognize_google(audio_text)`
+                text = r.recognize_google(recorded_audio, language=language_code)
+                if saved=="Yes":
+                    with open('my_speech.txt', 'w') as f:
+                        f.write(text)
+                return "Google Speech Recognition thinks you said :" + text
+            except sr.UnknownValueError:
+                return "Google Speech Recognition could not understand audio"
+            except sr.RequestError as e:
+                return f"Could not request results from Google Speech Recognition service; {e}"
 
-if st.button('Clear Cache'):
-    with open("code.txt","w") as file:
-        file.write("")
+        else:
+            # recognize speech using Sphinx
+            try:
+                text = r.recognize_sphinx(recorded_audio, language=language_code)
+                if saved=="Yes":
+                    with open('my_speech.txt', 'w') as f:
+                        f.write(text)
+                return "Sphinx thinks you said :" + text
+            except sr.UnknownValueError:
+                return "Sphinx could not understand audio"
+            except sr.RequestError as e:
+                return f"Sphinx error; {e}"
 
-if st.button('Save Editor Code'):
-    with open('editor.txt',"w") as file:
-        file.write(content)
+def main():
+    st.set_page_config(layout="wide")
+
+    # Create a sidebar
+    st.sidebar.title("Settings")
+    
+    # Choose Speech Recognition API
+    api = st.sidebar.selectbox("Choose Speech Recognition API", ["Google Web Speech API", "CMU Sphinx"])
+    st.sidebar.info("""
+        There are more APIs like : Microsoft Bing Speech, Google Cloud Speech, Houndify by SoundHound, 
+        IBM Speech to Text and Wit.ai but they all require authentication with either an API key 
+        or a username/password combination.
+    """)
+    # Choose to save text output or not
+    saved = st.sidebar.radio("Do you wish to save what you just have said ?", ["Yes", "No"])
+    
+    # Choose Language
+    lang = st.sidebar.selectbox("Choose the language you speak with", list(language_codes.keys()))
+
+    
+    st.title("Speech Recognition App")
+    st.write("Click on the microphone to start speaking:")
+
+    # add a button to trigger speech recognition
+    if st.button("Start Recording"):
+        pause_state = False  # Ensure recognition is not paused
+        text = transcribe_speech(api, saved)
+        st.write(text)
+
+if __name__ == "__main__":
+    main()
